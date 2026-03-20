244655f8f7c18358884617323f24522988f9fc29
Joe Marshall <joeymarshall94@gmail.com>
2026-03-20 12:24:27 +0000

    Update Stripe API version to 2025-08-27.basil

--- app/api/stripe-session/route.tsx
+++ app/api/stripe-session/route.tsx
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-06-30.basil', // Match your project's API version
+  apiVersion: '2025-08-27.basil', // Match your project's API version
 });
 
 export async function GET(req: NextRequest) {
